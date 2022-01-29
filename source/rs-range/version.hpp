cf9bd676d1b1d9c8ca94f53d841db6a5b8ed1689
Ross Smith <rosssmith@me.com>
2022-01-30 10:02:04 +1100

    Fix includes in rs-range.hpp

--- source/rs-range/version.hpp
+++ source/rs-range/version.hpp
@@ -8,11 +8,11 @@
 namespace RS::Range {
 
     inline std::array<int, 3> version() noexcept {
-        return {{ 0, 3, 1 }};
+        return {{ 0, 3, 2 }};
     }
 
     inline std::string version_string() {
-        return "0.3.1";
+        return "0.3.2";
     }
 
 }
